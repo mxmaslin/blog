cbc6564cbad9b1c885ad73fca9757f38af4bd40b
Maxim Maslin <zapzarap@yandex.ru>
2019-05-05 10:44:57 +0300

    add admin

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog_app.models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'published',
+                    'status')
+    list_filter = ('status', 'created', 'published', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'published'
+    ordering = ['status', 'published']
+
+
+admin.site.register(Post, PostAdmin)
