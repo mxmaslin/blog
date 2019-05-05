187aa485e63f7114dfce2b8d396319c46ffb640a
Maxim Maslin <zapzarap@yandex.ru>
2019-05-05 21:15:37 +0300

    fix reverse for post_detail

--- blog_app/models.py
+++ blog_app/models.py
@@ -38,7 +38,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse(
-            'blog:post_detail',
+            'post_detail',
             args=[
                 self.published.year,
                 self.published.strftime('%m'),
